d491d71e9110ba691c66530ba4e5118b81c4cdf7
Chandler Shortlidge <chandlershortlidge@gmail.com>
2026-01-27 15:47:07 +0100

    full RAG system ready

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,6 +1,11 @@
 
-from youtube_transcript_api import YouTubeTranscriptApi
-ytt_api = YouTubeTranscriptApi()
+import langchain
+import langchain_openai
+
+from langchain_openai import ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from langchain_core.documents import Document
+
 
 import json
 
@@ -25,12 +30,46 @@ def create_json(filename, data):
         json.dump(data, f)
 
 
-def load_json(filename, data):
-    with open(filename, "r") as f:
-        json.dump(data, f)
-
-
 def load_json(filename):
     with open(filename, "r") as f:
         data = json.load(f)
     return data 
+
+
+
+
+"""RAG PIPELINE"""
+
+
+
+def transcript_pipeline(json_file):
+    """Pull the raw transcript from the JSON file"""
+    json_doc = load_json(json_file)
+    raw_transcript = json_doc["transcript"]
+
+    """Build a new dictionary called "metadata" that extracts the keys from the json file"""
+    metadata = {
+    "video_id": json_doc["video_id"],
+    "title": json_doc["title"],
+    "author": json_doc["author"],
+    "difficulty": json_doc["difficulty"],
+    "exercise_type": json_doc["exercise_type"]
+}
+
+    """Make the raw transcript readable by a human for chunking using LLM"""
+    llm = ChatOpenAI(model='gpt-4o')
+    response = llm.invoke(f"Edit this document. Make the transcript clean and readable by a human. Remove all line break characters '/n'. Clean all typos.: {raw_transcript}")
+    return response.content, metadata
+
+
+
+def split_text_add_metadata(clean_transcript, metadata):
+    """Split the text and chunk it"""
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_text(clean_transcript)
+
+    """Add metadata"""
+    documents = [Document(page_content=chunk, metadata=metadata) for chunk in chunks]
+
+    return documents
+
