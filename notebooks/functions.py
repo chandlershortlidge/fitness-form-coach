4f64e03ee8f8a8b0c229ed94ed08ef676293c244
Chandler Shortlidge <chandlershortlidge@gmail.com>
2026-01-29 16:16:11 +0100

    I rewrote the youtube transcription pipeline. This cleans and makes more efficent yesterday's work, where I cleaned the raw youtube transcriptions using GPT in the rag_pipeline, then added those to the vectorstore embeddings. But I realized that adding new transcripts would force me to rerun all previous transcripts in the notebook in order to initalize their respective variables when embedding with in all_docs, costing API calls.
    So I created the clean_and_save_transcript function in functions.py, which takes the raw transcript filepath (a json dictionary), reads it, cleans the transcript it with GPT, adds the clean transcript back to the dictionary, then exports and saves the the clean json dict to a new filepath in /processed (this is done manually).

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -5,7 +5,8 @@ import langchain_openai
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_core.documents import Document
-
+from youtube_transcript_api import YouTubeTranscriptApi
+ytt_api = YouTubeTranscriptApi()
 
 import json
 
@@ -19,24 +20,51 @@ def get_transcript(video_id):
     return joined
 
 
-def get_metadata(video_id, difficulty, title, author, exercise_type, transcript):
+def write_metadata(video_id, difficulty, title, author, exercise_type, transcript):
     full_url = f"https://www.youtube.com/watch?v={video_id}"
     metadata = {"video_id": video_id, "title": title, "author": author, "difficulty": difficulty, "exercise_type": exercise_type, "transcript": transcript, "full_url": full_url}
     return metadata
 
 
-def create_json(filename, data):
+def write_json(filename, data):
+    """Filename = /Users/your/file/path/name_of_file.json
+    data = metadata"""
     with open(filename, "w") as f:
         json.dump(data, f)
 
 
-def load_json(filename):
+def read_json(filename):
     with open(filename, "r") as f:
         data = json.load(f)
     return data 
 
 
+# Read that dictionary (data) back from the JSON
+def clean_and_save_transcript(filepath_in, filepath_out):
+# pull the raw_transcript fro the raw_file json dictionary
+    # Step 1: Read the dictionary
+    data = read_json(filepath_in)
+    # Step 2: Get the raw transcript from it
+    raw_transcript = data["transcript"]
+    # Step 3: Clean it with GPT
+    llm = ChatOpenAI(model='gpt-4o')
+    # invoke LLM to clean the and edit the raw_transcript, producing cleaned_text
+    response = llm.invoke(f"Edit this document {raw_transcript}. Make the transcript clean and readable by a human." 
+                        "Remove all line break characters '/n'. Clean all typos. Return ONLY the transcript." 
+                        "NO comment from you.")
+    # Step 4: Add cleaned text to dictionary
+    cleaned_text = response.content # .content turns the response into a string
+    data["clean_transcript"] = cleaned_text
+    # remove raw transcript from metadata
+    data.pop('transcript')
+    # save and export
+    cleaned_json_file = write_json(filepath_out, data)
+    return cleaned_json_file
 
+# example: 
+# cleaned_bench_json = clean_and_save_transcript(filepath_in, filepath_out) 
+# raw_file = bench_json_raw = read_json("/Users/chandlershortlidge/Desktop/Ironhack/fitness-form-coach/data/transcripts/nippard_bench_dict.json")
+# metadata = bench_metadata = write_metadata(video_id="vcBig73ojpE", difficulty="intermediate", title="How To Get A Huge Bench Press with Perfect Technique", author="Jeff Nippard", exercise_type="bench_press", transcript=transcript)
 
 """RAG PIPELINE"""
 
@@ -44,7 +72,7 @@ def load_json(filename):
 
 def transcript_pipeline(json_file):
     """Pull the raw transcript from the JSON file"""
-    json_doc = load_json(json_file)
+    json_doc = read_json(json_file)
     raw_transcript = json_doc["transcript"]
 
     """Build a new dictionary called "metadata" that extracts the keys from the json file"""
@@ -53,12 +81,12 @@ def transcript_pipeline(json_file):
     "title": json_doc["title"],
     "author": json_doc["author"],
     "difficulty": json_doc["difficulty"],
-    "exercise_type": json_doc["exercise_type"]
+    "exercise_type": json_doc["exercise_type"],
 }
 
     """Make the raw transcript readable by a human for chunking using LLM"""
     llm = ChatOpenAI(model='gpt-4o')
-    response = llm.invoke(f"Edit this document. Make the transcript clean and readable by a human. Remove all line break characters '/n'. Clean all typos.: {raw_transcript}")
+    response = llm.invoke(f"Edit this document. Make the transcript clean and readable by a human. Remove all line break characters '/n'. Clean all typos. Return ONLY the transcript. NO comment from you.: {raw_transcript}")
     return response.content, metadata
 
 
